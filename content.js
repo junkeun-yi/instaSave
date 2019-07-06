dd17323b435eaf1c821195aa4d86b63a0fb88e7c
Akash Gokul <akashgokul99@Admins-MacBook-Pro.local>
2019-07-06 13:39:13 -0700

    Added null functionality

--- content.js
+++ content.js
@@ -1,6 +1,9 @@
-img_src = document.querySelector("meta[property='og:image']").getAttribute("content");
-if (img_src == null){
-  alert("Null Image")
-  return()
+window.onload = function(){
+  img_src = document.querySelector("meta[property='og:image']").getAttribute("content");
+  if(img_src == null){
+    alert("Null Image");
+  }
+  else{
+    window.open(img_src);  
+  }
 }
-window.open(img_src)
