5410992fd5a60fbc312615a9c3b0d18509cba64c
Akash Gokul <akashgokul99@Admins-MacBook-Pro.local>
2019-07-06 14:01:10 -0700

    Changed manifest for content to run at doc_end

--- content.js
+++ content.js
@@ -1,7 +1,5 @@
-window.onload = function(){
-  meta_tag = document.querySelector("meta[property='og:image']");
-  if(meta_tag != null){
-    img_src = meta_tag.getAttribute("content");
-    window.open(img_src);
-  }
+meta_tag = document.querySelector("meta[property='og:image']");
+if(meta_tag != null){
+  img_src = meta_tag.getAttribute("content");
+  window.open(img_src);
 }
