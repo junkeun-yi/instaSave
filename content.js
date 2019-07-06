a0c681a1ea92167c43138dee2c97078088a45f37
Junkeun Yi <junkeun.yi@berkeley.edu>
2019-07-06 13:08:01 -0700

    jk_test

--- content.js
+++ content.js
@@ -1,3 +1,9 @@
 alert("Hello from your Chrome extension!")
-img_src = document.getElementsByName("og:image")[0].getAttribute('content')
-alert(img_src)
+img_src = document.head.querySelector('[property="og:image"]').content;
+if (img_src == null){
+  alert("NULL")
+}
+else{
+  popup = window.open();
+  popup.document.write(img_src);
+}
