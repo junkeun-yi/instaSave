1d5da772309227f18e62e49f745a79b6330158e4
Akash Gokul <akashgokul99@Admins-MacBook-Pro.local>
2019-07-06 13:32:18 -0700

    Deleted Hello alert

--- content.js
+++ content.js
@@ -1,5 +1,2 @@
-alert("Hello from your Chrome extension!")
 img_src = document.querySelector("meta[property='og:image']").getAttribute("content");
-if (img_src != null){
-  window.open(img_src)
-}
+window.open(img_src)
