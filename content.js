b5ce9da870c23be8cd4f5bc61f57f25ed404f554
Akash Gokul <akashgokul99@Admins-MacBook-Pro.local>
2019-07-06 13:48:04 -0700

    Fixed the null case

--- content.js
+++ content.js
@@ -1,9 +1,7 @@
 window.onload = function(){
-  img_src = document.querySelector("meta[property='og:image']").getAttribute("content");
-  if(img_src == null){
-    alert("Null Image");
-  }
-  else{
-    window.open(img_src);  
+  meta_tag = document.querySelector("meta[property='og:image']");
+  if(meta_tag != null){
+    img_src = meta_tag.getAttribute("content");
+    window.open(img_src);
   }
 }
