3dd80cd8544057fc63b427600ad69e1bf95a7c1b
junkeun-yi <32083363+junkeun-yi@users.noreply.github.com>
2019-07-06 13:28:40 -0700

    fix errors

--- content.js
+++ content.js
@@ -1,3 +1,5 @@
 alert("Hello from your Chrome extension!")
 img_src = document.querySelector("meta[property='og:image']").getAttribute("content");
-window.open(img_src)
+if (img_src != null){
+  window.open(img_src)
+}
